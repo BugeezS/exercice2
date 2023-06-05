b350365b56a57e52c0b4f9816bd662a4e0b7b539
BugeezS <brunolb.lafont@gmail.com>
2023-06-05 11:30:50 +0200

    add array

---
+++ array.php
@@ -0,0 +1,16 @@
+<?php
+
+$countries = array( 'belgium','France','Germany','Netherlands','Ukraine');
+echo $countries[2];
+
+
+
+$familly = array ("Pascale","Marc","Tziga","Bruno");
+print_r($familly);
+
+$recipes = array ("poulet","poulet","poulet","pulet");
+print_r($recipes);
+
+
+
+?>
