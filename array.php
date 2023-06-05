d550d26b4d4ecc0c25a88f5f4b40b38b8bcd7ef4
BugeezS <brunolb.lafont@gmail.com>
2023-06-05 12:08:45 +0200

    array

--- array.php
+++ array.php
@@ -1,16 +1,34 @@
 <?php
 
-$countries = array( 'belgium','France','Germany','Netherlands','Ukraine');
-echo $countries[2];
+// $me = array(
+// 	'age'=> 45 ,
+// 	'firstname' => 'Alexandre' ,
+// 	'lastname'  => 'Plennevaux' ,
+// 	'favourite_movies' => array('My Own Private Idaho', 'Her', 'Matrix')
+// );
+// $me['hobbies'] = array("jv","manger","lire","dormir");
+// $me['mother'] = array();
+
+// echo '<pre>';
+// print_r($me);
+// echo '</pre>';
 
 
+$web_development = array(
+    'frontend' => array(),
+    'backend' => array()
+);
 
-$familly = array ("Pascale","Marc","Tziga","Bruno");
-print_r($familly);
+array_push($web_development['frontend'], 'xhtml');
+array_push($web_development['backend'], 'Ruby on Rails');
+array_push($web_development['frontend'], 'CSS');
+array_push($web_development['frontend'], 'Flash');
+array_push($web_development['frontend'], 'JavaScript');
 
-$recipes = array ("poulet","poulet","poulet","pulet");
-print_r($recipes);
+$web_development['frontend'][array_search('xhtml', $web_development['frontend'])] = 'html';
 
+unset($web_development['frontend'][array_search('Flash', $web_development['frontend'])]);
 
+print_r($web_development);
 
 ?>
